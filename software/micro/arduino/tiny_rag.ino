0833c09ba7f386945e305b38ca1d142584af8614
bms@barwap.com <bms@barwap.com>
2012-09-02 19:37:16 +0100

    Attiny working with RTC using i2c

--- software/micro/arduino/tiny_rag.ino
+++ software/micro/arduino/tiny_rag.ino
@@ -28,8 +28,10 @@
 
 #include <avr/sleep.h>
 #include <avr/wdt.h>
-#include <Time.h>  
+#include <TinyWireM.h>  
 
+#define DS1307_ADDRESS 0x68
+byte zero = 0x00; //workaround for issue #527
 
 #ifndef cbi
 #define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
@@ -38,20 +40,29 @@
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
 #endif
 
-#define RED_PIN 2
-#define AMBER_PIN 1
-#define GREEN_PIN 0
-#define PIN_TOUCH 4
+#define RED_PIN 1
+#define AMBER_PIN 4
+#define GREEN_PIN 3
+// Pin 0 SDA
+// Pin 2 SCL
+#define PIN_TOUCH 5
 
 #define NUMBER_DELAY 500
 #define MAX_LOOPS 2
 
 volatile boolean f_wdt = 1;
-int calibration = 0;
-unsigned long t;
-unsigned long remainderMills;
-unsigned long oldMills;
-unsigned long newMills;
+int touch_calibration = 0;
+
+struct {
+  int second;
+  int minute;
+  int hour; //24 hour time
+  int weekDay;
+  int monthDay;
+  int month;
+  int year;
+} rtcDate;
+
 
 void setup() {                
   cbi(ADCSRA,ADEN);  // switch Analog to Digitalconverter OFF
@@ -61,44 +72,42 @@ void setup() {
   ragPinsToOutput();
   // Traditional Flash test
   flashRAG();
- 
-  // Lets count to twelve
-  // This gives us time to realease the cufflinks before calibration 
-  // ... so th eback can be screwed in before calibration starts.
-   for (int l = 0; l < 13; l++) {
+   // Lets count to twelve
+  // CHANGE TO 13(12) FOR PRODUCTION 
+  // This gives us time to realease the cufflinks before touch_calibration 
+  // ... so th eback can be screwed in before touch_calibration starts.
+   for (int l = 0; l < 6; l++) {
      showNumber(l);
    }
-  powerDown();   // Want calibration down with RAGs off
+  powerDown();   // Want touch_calibration down with RAGs off
 
-  // Get Calibration Value
+  // Get touch_calibration Value
   for (int i = 0; i < 8; i++) {
-    calibration += chargeTime();
+    touch_calibration += chargeTime();
     delay(20);
   }
-  calibration = (calibration + 4) / 8;
+  touch_calibration = (touch_calibration + 4) / 8;
 
   ragPinsToOutput();
   flashRAG();
   powerDown();
+  
+  TinyWireM.begin();
+
+  setRtcTime(12, 9, 1, 12, 0); //MUST CONFIGURE IN FUNCTION 01-09-2012, 12:00
+ // Sleep set-up
   setup_watchdog(8);
-  //  t = 1343261200;
-  t = 0;
 }
 
 
 void loop() {
-  oldMills = millis();
-  int addSeconds = 0;
   int touches = 0;
-
   if (f_wdt==1) {  // wait for timed out watchdog / flag is set when a watchdog timeout occurs
     f_wdt=0;       // reset flag
-
-    // If touch sense then ... 
+    // If touch sensed then ... 
     if (touched())  { 
       ragPinsToOutput();
       touches = getTouches(1, 1, 6, MAX_LOOPS);
-
       switch (touches) {
         case 1:
           junctionRAG();
@@ -119,93 +128,92 @@ void loop() {
           setLocalDate();
           break;
       }
-  
       powerDown();
     }
-
     system_sleep();
-
-    // I slept for four thousand milliseconds
-    // Add milliseconds it took to run above
-    // psudeo code + fudgae factor to add.... 
-    newMills = millis();
-    remainderMills = remainderMills + (newMills-oldMills) + (4000);
-
-    t = t + (remainderMills / 1000);
-    remainderMills = remainderMills % 1000;
-
   }
 }
 
+
+void junctionRAG() {
+  rag(1,0,0,2000);
+  rag(1,1,0,2000);
+  rag(0,0,1,7000);
+  rag(0,1,0,3000);
+  rag(1,0,0,2000);  
+}
+
+void pelicanRAG()  {
+  rag(1,0,0,2000);
+  for (int l = 0; l < 4; l++) {
+    rag(0,1,0,500);
+    rag(0,0,0,500);
+  }
+  rag(0,0,1,7000);
+  rag(0,1,0,3000);
+  rag(1,0,0,2000);
+}  
+
 void showTime()  {
-  showNumber(hour(t)%12);
+  getRtcTime();
+  showNumber(rtcDate.hour%12);
   delay(NUMBER_DELAY*2);
-  showNumber(minute(t)/10);
+  showNumber(rtcDate.minute/10);
   delay(NUMBER_DELAY*2);
-  showNumber(minute(t)%10);
-//        delay(NUMBER_DELAY*2);
-//        showNumber(second(t)/10);
-//        delay(NUMBER_DELAY*2);
-//        showNumber(second(t)%10);
+  showNumber(rtcDate.minute%10);
 }
 
 void showDate()  {
-  showNumber(day(t)/10);
+  getRtcTime();
+  showNumber(rtcDate.monthDay/10);
   delay(NUMBER_DELAY*2);
-  showNumber(day(t)%10);
+  showNumber(rtcDate.monthDay%10);
   delay(NUMBER_DELAY*2);
-  showNumber(month(t));
+  showNumber(rtcDate.month);
   delay(NUMBER_DELAY*2);
-  showNumber((year(t)%100)/10);
+  showNumber((rtcDate.year%100)/10);
   delay(NUMBER_DELAY*2);
-  showNumber((year(t)%100)%10);
+  showNumber((rtcDate.year%100)%10);
 }
 
-
 void setLocalTime() {
-  // setTime(int hr,int min,int sec,int day, int month, inti yr); 
-  int hr, min, min_tens, min_units, d, m, y;
-  d = day(t);
-  m = month(t);
-  y = year(t);
-
+  int hour, minute;
+  getRtcTime();
+  // Get value by touching
   signalSettingMode();
-  hr = getTouches(hour(t)%12, 0, 11, MAX_LOOPS);
+  hour = getTouches(rtcDate.hour%12, 0, 11, MAX_LOOPS);
   signalSettingMode();
-  min_tens = getTouches(minute(t)/10, 0, 5, MAX_LOOPS);
+  minute = 10 * getTouches(rtcDate.minute/10, 0, 5, MAX_LOOPS);
   signalSettingMode();
-  min_units = getTouches(minute(t)%10, 0, 9, MAX_LOOPS);
-  setTime(hr,(min_tens*10)+min_units,0,d,m,y);
-  t = now();
+  minute = minute + getTouches(rtcDate.minute%10, 0, 9, MAX_LOOPS);
+  // Update RTC
+  setRtcTime(rtcDate.year, rtcDate.month, rtcDate.monthDay, hour, minute);
 }
 
 void setLocalDate() {
   // setTime(int hr,int min,int sec,int day, int month, int yr); 
-  int hr, mn, am_or_pm, d_tens, d_units, m, y_tens, y_units;
-  hr = hour(hr)%12;
-  mn = minute(t);
+  int hour, year, month, monthDay, am_or_pm;
+  getRtcTime();
+  // Get value by touching
+  signalSettingMode();
+  am_or_pm = getTouches(rtcDate.hour/12, 0, 1, MAX_LOOPS);
+  hour = (rtcDate.hour%12) + (12 * am_or_pm);
+  signalSettingMode();
+  monthDay = getTouches(rtcDate.monthDay/10, 0, 3, MAX_LOOPS);
+  signalSettingMode();
+  monthDay = monthDay + getTouches(rtcDate.monthDay%10, 0, 9, MAX_LOOPS);
+  signalSettingMode();
+  month = getTouches(rtcDate.month, 1, 12, MAX_LOOPS);
+  signalSettingMode();
+  year = 10 * getTouches((rtcDate.year%100)/10, 0, 9, MAX_LOOPS);
+  signalSettingMode();
+  year = year + getTouches((rtcDate.year%100)%10, 0, 9, MAX_LOOPS);
+  signalSettingMode();
+  // Update RTC
+//  setTime(rtcDate.year,mn,0,(d_tens*10)+d_units,m,2000+(y_tens*10)+y_units);
+  setRtcTime(year + ((rtcDate.year/100) * 100), month, monthDay, rtcDate.hour, rtcDate.minute);
 
-  signalSettingMode();
-  am_or_pm = getTouches(hour(t)/12, 0, 1, MAX_LOOPS);
-  hr = (hour(hr)%12) + (12 * am_or_pm);
-  signalSettingMode();
-  d_tens = getTouches(day(t)/10, 0, 3, MAX_LOOPS);
-  signalSettingMode();
-  d_units = getTouches(day(t)%10, 0, 9, MAX_LOOPS);
-  signalSettingMode();
-  m = getTouches(month(t), 1, 12, MAX_LOOPS);
-  signalSettingMode();
-  y_tens = getTouches((year(t)%100)/10, 0, 9, MAX_LOOPS);
-  signalSettingMode();
-  y_units = getTouches((year(t)%100)%10, 0, 9, MAX_LOOPS);
-  signalSettingMode();
-
-  setTime(hr,mn,0,(d_tens*10)+d_units,m,2000+(y_tens*10)+y_units);
-  t = now();
 }
-
-
-
 
 
 byte getTouches(byte touches, byte min_touch, byte max_touch, byte max_loops) {
@@ -229,6 +237,48 @@ byte getTouches(byte touches, byte min_touch, byte max_touch, byte max_loops) {
     }
   }
 }
+
+void getRtcTime() {
+  TinyWireM.beginTransmission(DS1307_ADDRESS);
+  TinyWireM.send(zero);
+  TinyWireM.endTransmission();
+
+  TinyWireM.requestFrom(DS1307_ADDRESS, 7);
+
+  rtcDate.second = bcdToDec(TinyWireM.receive());
+  rtcDate.minute = bcdToDec(TinyWireM.receive());
+  rtcDate.hour = bcdToDec(TinyWireM.receive() & 0b111111); //24 hour time
+  rtcDate.weekDay = bcdToDec(TinyWireM.receive()); //0-6 -> sunday - Saturday
+  rtcDate.monthDay = bcdToDec(TinyWireM.receive());
+  rtcDate.month = bcdToDec(TinyWireM.receive());
+  rtcDate.year = bcdToDec(TinyWireM.receive());
+}
+
+
+void setRtcTime(byte year, byte month, byte monthDay, byte hour, byte minute) {
+  TinyWireM.beginTransmission(DS1307_ADDRESS);
+  TinyWireM.send(zero); //stop Oscillator
+  TinyWireM.send(decToBcd(0)); // Always ^Dhalfway start through the minute ?
+  TinyWireM.send(decToBcd(minute));
+  TinyWireM.send(decToBcd(hour));
+  TinyWireM.send(decToBcd(1)); // WeekDay not used
+  TinyWireM.send(decToBcd(monthDay));
+  TinyWireM.send(decToBcd(month));
+  TinyWireM.send(decToBcd(year));
+  TinyWireM.send(zero); //start 
+  TinyWireM.endTransmission();
+}
+
+byte decToBcd(byte val){
+// Convert normal decimal numbers to binary coded decimal
+  return ( (val/10*16) + (val%10) );
+}
+
+byte bcdToDec(byte val)  {
+// Convert binary coded decimal to normal decimal numbers
+  return ( (val/16*10) + (val%16) );
+}
+
 
 
 void signalSettingMode() {
@@ -254,27 +304,36 @@ void flashRAG() {
   rag(0,0,0,0);
 }
 
-void junctionRAG() {
-  rag(1,0,0,2000);
-  rag(1,1,0,2000);
-  rag(0,0,1,7000);
-  rag(0,1,0,3000);
-  rag(1,0,0,2000);  
+void showNumber(int n) {
+  // I am never goes to change the powers
+  // 10, 5, 0
+  // max value is 19
+  int remainder = n;
+  int green_v, amber_v, red_v;
+  if (n<20) {
+    green_v = n / 10;
+    remainder = remainder % 10;
+    amber_v = remainder / 5;
+    red_v = remainder % 5;
+    rag(0,amber_v,green_v,(NUMBER_DELAY*2));
+    for (int l = 0; l < red_v; l++) {
+      rag(1,amber_v,green_v,NUMBER_DELAY);
+      rag(0,amber_v,green_v,NUMBER_DELAY);
+    }
+/*    if ((red_v<2) && (n!=0)) {
+      rag(0,amber_v,green_v,(NUMBER_DELAY*(2-red_v)));
+    }
+    // A lack of LEDs will make it hard to work out 10 or 01 or 100  */
+    if (n==0) {
+      rag(1,1,1,(NUMBER_DELAY));
+    }
+    // Pause at the end so it is clear not 
+    rag(0,0,0,(NUMBER_DELAY));
+  }
+
 }
 
-void pelicanRAG()  {
-  rag(1,0,0,2000);
-  for (int l = 0; l < 4; l++) {
-    rag(0,1,0,500);
-    rag(0,0,0,500);
-  }
-  rag(0,0,1,7000);
-  rag(0,1,0,3000);
-  rag(1,0,0,2000);
-}  
-
-
-void rag(boolean red, boolean amber, boolean green, float period)  {
+void rag(boolean red, boolean amber, boolean green, unsigned int period)  {
   if (red) {
     digitalWrite(RED_PIN, HIGH);
   }
@@ -299,42 +358,11 @@ void rag(boolean red, boolean amber, boolean green, float period)  {
   delay((int)period);
 }
 
-void showNumber(int n) {
-  // I am never goes to change the powers
-  // 10, 5, 0
-  // max value is 19
-  int remainder = n;
-  int green_v, amber_v, red_v;
-  if (n<20) {
-    green_v = n / 10;
-    remainder = remainder % 10;
-    amber_v = remainder / 5;
-    red_v = remainder % 5;
-    rag(0,amber_v,green_v,(NUMBER_DELAY*2));
-    for (int l = 0; l < red_v; l++) {
-      rag(1,amber_v,green_v,NUMBER_DELAY);
-      rag(0,amber_v,green_v,NUMBER_DELAY);
-    }
-    if ((red_v<2) && (n!=0)) {
-      rag(0,amber_v,green_v,(NUMBER_DELAY*(2-red_v)));
-    }
-    // A lack of LEDs will make it hard to work out 10 or 01 or 100  
-    if (n==0) {
-      rag(1,1,1,(NUMBER_DELAY));
-    }
-    // Pause at the end so it is clear not 
-    rag(0,0,0,(NUMBER_DELAY));
-  }
-
-}
-
-
 void powerDown()  {
   rag(0,0,0,0);
   // Save Power
   ragPinsToInput();
 }
-
 
 void ragPinsToInput()  {
   pinMode(RED_PIN, INPUT);
@@ -350,7 +378,7 @@ void ragPinsToOutput()  {
 
 bool touched() {
   int new_charge = chargeTime(); 
-  if (new_charge > calibration) {
+  if (new_charge > touch_calibration) {
     return true;
   }
   else {
