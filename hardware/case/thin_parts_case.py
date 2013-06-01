32284de71a394e8ab10be0552dacba3db2206535
bms@barwap.com <bms@barwap.com>
2013-06-01 20:22:47 +0100

    Updating thin parts

--- hardware/case/thin_parts_case.py
+++ hardware/case/thin_parts_case.py
@@ -16,11 +16,13 @@ min_embossed_detail = 0.3
 pcb_diameter = 14.5
 pcb_thickness = 0.4
 pcb_gap =0.25
-tooth_depth = 0.35
+tooth_depth = 0.7
+tooth_height = 1.4
+tooth_pitch = 1.4
 
 #top_inside_diameter = 15.8
 top_inside_diameter = pcb_diameter + 2*tooth_depth + 2*pcb_gap
-top_outside_diameter = 15.8 + (2 * min_wall_free)
+top_outside_diameter = top_inside_diameter + (2 * min_wall_free)
 top_height = 8.1
 led_hole_dia = 1.4
 led_gap = 3
@@ -60,11 +62,11 @@ def led_hole():
     return cylinder(r=led_hole_dia/2, h=min_wall_free)
 
 def hood():
-    hood_d = led_hole_dia + (min_wall_free*2)
+    hood_d = led_hole_dia + (min_wall_free)
     hood_r = hood_d/2
-    c = cylinder( r=hood_r, h=min_wall_free)
-    c = c - cylinder( r=led_hole_dia/2, h=min_wall_free)
-    t = translate([-hood_r,-hood_r,min_wall_free])(rotate([0, 90, 0])(triangle(hood_d,min_wall_free,hood_d)))
+    c = cylinder( r=hood_r, h=min_wall_free/2)
+    c = c - cylinder( r=led_hole_dia/2, h=min_wall_free/2)
+    t = translate([-hood_r,-hood_r,min_wall_free/2])(rotate([0, 90, 0])(triangle(hood_d,min_wall_free/2,hood_d)))
     return c - t
 
 def boarder():
@@ -90,40 +92,50 @@ def cuff_top():
     return top + h + b + p
 
 def top():
-    return cuff_side() + up(top_height-min_wall_free)(cuff_top())
+    return cuff_side() + up(top_height-min_wall_free)(cuff_top()) + plastic_connector_top(gap=0, reverse=True)
 
 
-def plastic_connector_top(gap=0, hole=True):
+def plastic_connector_top(gap=0, reverse=False):
     SEGMENTS = 120
-    inner_rad = plastic_connector_dia/2
-    section = screw_thread.default_thread_section( tooth_height=0.7, tooth_depth=tooth_depth)   
+    inner_rad = plastic_connector_dia/2 - gap
+    section = screw_thread.default_thread_section( tooth_height=tooth_height, tooth_depth=tooth_depth)   
     s = screw_thread.thread( outline_pts=section, inner_rad = inner_rad,
-                            pitch= 0.7, length=plastic_top_screw_height, segments_per_rot=SEGMENTS
-                            , neck_in_degrees=30, neck_out_degrees=30)
+                            pitch=tooth_pitch, length=plastic_top_screw_height, segments_per_rot=SEGMENTS, 
+                            neck_in_degrees=30, neck_out_degrees=30)
                         
     c = cylinder( r=inner_rad + .05, h=plastic_top_screw_height )
-    if hole==False:
-        top=s+c
+    if reverse==False:
+        top=s+c    
     else:
-        top=cylinder(r=top_inside_diameter/2, h=plastic_top_screw_height ) - (s+c)
-
+        top = up(pcb_gap/2)(cylinder(r=top_outside_diameter/2, h=plastic_top_screw_height-pcb_gap ))
+        top = top - (s+c)
     top = down(plastic_top_screw_height/2+pcb_thickness)(top)
     return up(top_height-min_wall_free-touch_pads_height-pcb_thickness)(top)
 
+def plastic_connector_bottom(gap=pcb_gap):
+# top_height
+    bottom_height = top_height-min_wall_free-touch_pads_height-pcb_thickness-plastic_top_screw_height
+    bottom_height_extra = bottom_height + min_wall_free
+    c = cylinder( r=top_inside_diameter/2 - gap, h=bottom_height )
+    c = c + cylinder( r=plastic_connector_dia/2 - gap, h=bottom_height_extra )
+    return c
 
-
-
+def plastic_connector():
+    return plastic_connector_bottom() + plastic_connector_top(0.35)
 
 def assembly():
    cutaway = cube([20,20,20])
    t = top()
-   c = plastic_connector_top()
+   c = plastic_connector()
    p = color(Green)(up(5.75)(pcb()))
 #   stork = stork()
 #   connector = connector()
 
 #  return top + stork + connector
-   return t + p + c - cutaway
+#   return t + p + c - cutaway
+   return t + c - cutaway
+#   r = cylinder(r=top_outside_diameter/2, h=plastic_top_screw_height )
+#   return plastic_connector_top(gap=0, reverse=True)
 
 
 
